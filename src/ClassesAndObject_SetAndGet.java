b9b1096050a69d8c123a9df0ee97bad898715cb1
Azat <azat.takhautdinov@gmail.com>
2022-09-26 15:26:38 +0500

    I got familiar with setters and getters.

---
+++ src/ClassesAndObject_SetAndGet.java
@@ -0,0 +1,39 @@
+public class ClassesAndObject_SetAndGet {
+    public static void main(String[] args) {
+        People person = new People();
+        person.setName("Имя");
+        person.setAge(12);
+        System.out.println("Значение в main от геттеров имени: "+person.getName());
+        System.out.println("Значение в main от геттеров возраст: "+person.getAge());
+        person.speak(); /* что дают сеттеры и геттеры? Внутри созданного класса мы можем
+        к примеру изменить переменную name на login и это никак не повлияет на метод main
+        и пользователь не заметит этого, он обращается к этой внутренней переменной
+        по этому же классу, но внутр. перем-я может как угодно изменятся */
+    }
+}
+class People {
+    private String name;
+    private int age;
+
+    public void setName(String userName){ // сеттер
+        if (userName.isEmpty()){
+            System.out.println("Ты ввел пустое имя");
+        } else {
+        name = userName;} //
+    }
+    public String getName(){ // геттер
+        return name;
+    }
+    public void setAge(int userAge){
+        if(userAge<0){
+            System.out.println("Ты ввел отрицательный возраст");
+        } else {
+        age = userAge;}
+    }
+    public int getAge(){
+        return age;
+    }
+    void speak() {
+        System.out.println("Привет, меня зовут "+name+", мне "+age+" лет");
+    }
+}
