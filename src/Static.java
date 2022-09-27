debe9079fe6c6a37cbe5932f8730238426d89a24
Azat <azat.takhautdinov@gmail.com>
2022-09-28 00:04:38 +0500

    Supplemented the material on the keyword "static".

--- src/Static.java
+++ src/Static.java
@@ -1,27 +1,36 @@
 public class Static {
     public static void main(String[] args) {
+        Humans1 h1 = new Humans1("Bob", 45);
+        Humans1 h2 = new Humans1("Gab", 42);
+
+        h2.howCountPeople();
+        h1.howCountPeople();
+
+        h1.description = "Bad";
         Humans1.description = "Nice";
         Humans1.getDescription();
-        Humans1 h1 = new Humans1("Bob", 45);
-        h1.description = "Bad";
     }
 }
 class Humans1 {
     private String name;
     private int age;
+    private static int countPeople; // эта переменная одна для всех объектов.
 
     public static String description;
 
     public Humans1(String name, int age){
         this.name = name;
         this.age = age;
-
+        countPeople++;
     }
     public void setName(String name){
         this.name = name;
     }
     public void setAge(int age){
         this.age = age;
+    }
+    public void howCountPeople(){
+        System.out.println("People: "+countPeople);
     }
     public static void getDescription(){
         System.out.println(description);
