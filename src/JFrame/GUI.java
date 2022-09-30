650532a79a4857df66c9d83d64f699d6e9c2b6d2
Azat <azat.takhautdinov@gmail.com>
2022-09-30 23:50:07 +0500

    Test work with the "Swing" library (super damp for now).

---
+++ src/JFrame/GUI.java
@@ -0,0 +1,13 @@
+package JFrame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI extends JFrame {
+
+    public void setBounds() {
+        super.setBounds(200, 200, 250,100);
+
+    }
+}
