bc3b366f6cda1e344ab5bd42941a9e9e3327ec7c
Azat <azat.takhautdinov@gmail.com>
2022-10-01 23:57:44 +0500

    update. new: setVisible

--- src/JFrame/GUI.java
+++ src/JFrame/GUI.java
@@ -5,6 +5,8 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class GUI extends JFrame {
+    JLabel name = new JLabel("Введите имя");
+    JTextField name_field = new JTextField();
 
     public void setBounds() {
         super.setBounds(200, 200, 250,100);
